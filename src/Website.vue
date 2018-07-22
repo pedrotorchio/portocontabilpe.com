7085c00571cc9310c034154d0a9fcfba185b1b72
pedrotorchio <pedrotorchio@gmail.com>
2018-07-22 13:56:53 -0300

    initial

---
+++ src/Website.vue
@@ -0,0 +1,29 @@
+<template>
+  <main id="website">
+    <navigation></navigation>
+    <hero></hero>
+    
+  </main>
+</template>
+
+<script>
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+
+export default {
+  name: 'Website',
+  components: {
+    Hero,
+    Navigation
+  }
+}
+</script>
+<style src="reset-css"></style>
+<style>
+.container {
+  max-width: 900px;
+  width: 100%;
+  margin: 0 auto;
+}
+</style>
+
