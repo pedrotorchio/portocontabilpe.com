5dc8f099720b8a2d1c5c2aa1d4f3be2310b64332
pedrotorchio <pedrotorchio@gmail.com>
2018-07-22 13:57:29 -0300

    hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,76 @@
+<script>
+export default {
+  name: 'Hero',
+  data() {
+    return {
+      cta: "Agende sua visita"
+    }
+  }
+}
+</script>
+<template>
+  <section id="hero">
+    <img class="bg-image" src="/static/bg1-overlay-v1.jpg" alt="">
+    <div class="logo-container container">
+      <img id="main-logo" src="/static/logo.png" alt="">
+    </div>
+    <div class="btn-container container">
+      <a class="cta" href="#contato">
+        {{ cta }}
+      </a>
+    </div>
+  </section>
+</template>
+<style scoped lang="scss">
+.bg-image {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 1;
+}
+#hero {
+  position:relative;
+  height: 600px;
+  width: 100%;
+  width: 100vw;
+  background-color: rgb(10, 48, 71);
+  display: flex;
+  flex-direction: column;
+}
+#main-logo {
+  max-width: 90%;
+}
+.logo-container {
+  flex: 1 1 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 2;
+}
+.btn-container {
+  flex: 0 1 5em;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 2;
+}
+.cta {
+  color: white;
+  font-family: sans-serif;
+  text-decoration: none;
+  border: 2px solid white;
+  border-radius: 4px;
+  padding: .4em;
+
+  transition-property: border-color, background-color;
+  transition-duration: .2s;
+  transition-timing-function: ease-in-out;
+}
+.cta:hover {
+  $color: rgb(228, 68, 46);
+  border-color: $color;
+  background-color: $color;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import Website from './Website';
+import LazyLoad from 'vue-lazyload';
 
 Vue.config.productionTip = false
 
+
+Vue.use(LazyLoad);
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+  el: '#website',
+  components: { Website },
+  template: '<Website/>'
 })
